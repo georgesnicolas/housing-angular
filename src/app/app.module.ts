233fd628ae768a4b20de3d425bc3af1ad361cdb5
gnicolas <gnicolas@GEORGES-LAPTOP>
2020-06-18 10:21:15 +0300

    manne fehim shi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PropertyCardComponent,
-    PropertyListComponent
+    PropertyListComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
