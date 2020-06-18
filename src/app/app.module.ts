7721f032b3e15ccf62729505609e9b2b03ee7d62
gnicolas <gnicolas@GEORGES-LAPTOP>
2020-06-18 10:11:24 +0300

    wrong test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-property-card',
+    //template: `<h1>I am a card</h1>`,
+    templateUrl: './property-card.component.html',
+    // styles:['h1 {color:red;}']
+    styleUrls: ['./property-card.component.scss']
+})
+export class PropertyCardComponent {
+    
+}
