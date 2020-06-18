7227d647ddf545d129eedb894d12a2724a700bda
gnicolas <gnicolas@GEORGES-LAPTOP>
2020-06-18 17:38:36 +0300

    changes part9

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-property-card',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
     styleUrls: ['./property-card.component.scss']
 })
 export class PropertyCardComponent {
-    
+    @Input() property: any
 }
--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -6,6 +6,87 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.scss']
 })
 export class PropertyListComponent implements OnInit {
+  
+  properties: Array<any> = [
+    {
+      "Id":1,
+      "Name":"Birla House",
+      "Type":"House",
+      "Price":12000
+    },
+    {
+      "Id":2,
+      "Name":"Barila Maakarouni",
+      "Type":"House",
+      "Price":1000000
+    },
+    {
+      "Id":2,
+      "Name":"Barila Maakarouni",
+      "Type":"House",
+      "Price":1000000
+    },
+    {
+      "Id":2,
+      "Name":"Barila Maakarouni",
+      "Type":"House",
+      "Price":1000000
+    },
+    {
+      "Id":2,
+      "Name":"Barila Maakarouni",
+      "Type":"House",
+      "Price":1000000
+    },
+    {
+      "Id":2,
+      "Name":"Barila Maakarouni",
+      "Type":"House",
+      "Price":1000000
+    },
+    {
+      "Id":2,
+      "Name":"Barila Maakarouni",
+      "Type":"House",
+      "Price":1000000
+    },
+    {
+      "Id":2,
+      "Name":"Barila Maakarouni",
+      "Type":"House",
+      "Price":1000000
+    },
+    {
+      "Id":2,
+      "Name":"Barila Maakarouni",
+      "Type":"House",
+      "Price":1000000
+    },
+    {
+      "Id":2,
+      "Name":"Barila Maakarouni",
+      "Type":"House",
+      "Price":1000000
+    },
+    {
+      "Id":2,
+      "Name":"Barila Maakarouni",
+      "Type":"House",
+      "Price":1000000
+    },
+    {
+      "Id":2,
+      "Name":"Barila Maakarouni",
+      "Type":"House",
+      "Price":1000000
+    },
+    {
+      "Id":2,
+      "Name":"Barila Maakarouni",
+      "Type":"House",
+      "Price":1000000
+    }
+]
 
   constructor() { }
 
